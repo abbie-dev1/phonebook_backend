75d021ed61e413b0aa57ee87ad2b9b9d4a91762a
Abbie <79112224+AbbieS2175@users.noreply.github.com>
2021-09-15 11:39:48 +0200

    deploying heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,10 @@ import { PhonebookModule } from './phonebook/phonebook.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
+		// "mongodb+srv://abbie:12345@cluster0.dpntl.mongodb.net/phonebookDB?retryWrites=true&w=majority"
+
+		MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),		
+		// MongooseModule.forRoot("mongodb://localhost:27017/PHONEBOOK_DB", { autoCreate: true }),
 		PhonebookModule,
 	],
 	controllers: [],
